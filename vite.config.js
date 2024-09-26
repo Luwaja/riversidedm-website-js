6a478da7e91611d5bdb3cfc5816720f4c05b0c20
Luke James <luke.james924@gmail.com>
2024-09-26 13:46:06 -0500

    Change vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/riversidedm-website-js/'
-})
+  build: { 
+    manifest: true,
+    outDir: "../backend/build",
+    emptyOutDir: true },
+  base: process.env.NODE_ENV === "production" ? "/static/" : "/",
+  root: "./",
+});
